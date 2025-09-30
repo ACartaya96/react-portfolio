2936b9f4a27c27e1dbfcd32cc7822a0a699e3cfd
Alexander Cartaya <alexandercartaya96@gmail.com>
2025-09-30 16:53:24 -0400

    GhPages Deploy Attempt 2

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
+const rawBase = import.meta.env.BASE_URL ?? "/";
+const basename = rawBase.replace(/\/$/, "");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>
